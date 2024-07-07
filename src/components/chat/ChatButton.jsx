9cfcca4d5c9bea107332933b9124522dd85a50c4
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2024-07-08 04:54:45 +0600

    update: Chatbit size

--- src/components/chat/ChatButton.jsx
+++ src/components/chat/ChatButton.jsx
@@ -4,7 +4,7 @@ export default function ChatButton({ toggleChat }) {
     return (
         <div
             onClick={toggleChat}
-            className="w-[80px] h-[100px] fixed bottom-6 right-6 z-30 flex justify-center text-white items-center cursor-pointer"
+            className="w-[52px] h-[100px] fixed bottom-6 right-4 z-30 flex justify-center text-white items-center cursor-pointer"
         >
             <img className="drop-shadow-lg inline-block" src="/images/chatBot.png" alt="bot" />
         </div>
