552c558ea4115ab43a5902f8923c201877facd56
Shaikot Kundu Akash <76767759+Developer-Shaikot@users.noreply.github.com>
2024-07-08 02:09:52 +0600

    update: responsive: Contact & BLog

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,15 @@
-const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116814.54860242897!2d90.30539351712466!3d23.802426885315903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8ada2664e21%3A0x3c872fd17bc11ddb!2sDaffodil%20International%20University!5e0!3m2!1sen!2sbd!4v1720253603117!5m2!1sen!2sbd" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+const iframe = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116814.54860242897!2d90.30539351712466!3d23.802426885315903!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8ada2664e21%3A0x3c872fd17bc11ddb!2sDaffodil%20International%20University!5e0!3m2!1sen!2sbd!4v1720253603117!5m2!1sen!2sbd" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
 
 const Contact = () => {
     return (
         <div className="py-8">
-            <div className="grid grid-cols-1 lg:grid-cols-2  ">
+            <div className="grid grid-cols-1 xl:grid-cols-2  gap-12">
                 <div className="">
                     <h2 className="text-2xl font-semibold mb-5">Google Location</h2>
                     <div dangerouslySetInnerHTML={{ __html: iframe }} />
                 </div>
 
-                <div className="mt-6 md:mt-0 pr-16">
+                <div className="mt-6 md:mt-0 pr-32 xl:pr-16">
                     <h2 className="text-2xl font-semibold mb-5">Have any plan in mind?</h2>
                     <form>
                         <div className="mb-5">
